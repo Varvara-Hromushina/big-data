15838101ebe41db51f7a8e4b637c9020436ed76e
Varvara-Hromushina <varvara.hromushina@yandex.ru>
2026-08-30 20:35:54 +0300

    Create task1.R

---
+++ lab1 - Введение в язык R/task1.R
@@ -0,0 +1,23 @@
+p <- c(7, 6, 5, 4)
+q <- c(0, 1, 2, 3)
+
+sum_pq <- p + q
+sum_pq
+
+diff_pq <- p - q
+diff_pq
+
+prod_pq <- p * q
+prod_pq
+
+div_pq <- p / q
+div_pq
+
+pow_pq <- p ^ q
+pow_pq
+
+pow_p <- p ^ 2
+pow_p
+
+pow_q <- q ^ 3
+pow_q
